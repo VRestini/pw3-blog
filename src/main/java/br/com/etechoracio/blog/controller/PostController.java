3a12d059cc7b1390ad6798feab31698f52adb4e6
Vitor <vitorsouzalibrelonrestini@gmail>
2024-08-13 10:11:17 -0300

    a

--- src/main/java/br/com/etechoracio/blog/controller/PostController.java
+++ src/main/java/br/com/etechoracio/blog/controller/PostController.java
@@ -3,6 +3,7 @@ package br.com.etechoracio.blog.controller;
 import br.com.etechoracio.blog.entity.Post;
 import br.com.etechoracio.blog.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +25,14 @@ public class PostController {
     }
 
     @GetMapping("/{id}")
-    public Optional<Post> buscarPorId(@PathVariable Long id){
-        return repository.findById(id);
+    public ResponseEntity<Post> buscarPorId(@PathVariable Long id)
+    {
+        var resposta = repository.findById(id);
+        if (resposta.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(resposta.get());
     }
+
+
 }
